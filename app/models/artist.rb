21bbfd92ef6202cbab6d848ab2ae3d77378d2cf5
FranciscoAvila <avila.fcojavier@gmail.com>
2017-10-31 23:29:04 +0000

    update img ok

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
     belongs_to :genre
     has_many :songs
+    
+    has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
+    
 end
